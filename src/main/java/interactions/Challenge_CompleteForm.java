ca4ef07add61544f1b6504b7d34c194d42d0207d
Sai Charan Koganti <charan.koganti@gmail.com>
2025-06-16 11:47:03 -0500

    TextBox Challenge

--- src/main/java/interactions/Challenge_CompleteForm.java
+++ src/main/java/interactions/Challenge_CompleteForm.java
@@ -23,6 +23,34 @@ public class Challenge_CompleteForm extends Main {
      * And click the Submit button
      */ 
 
+    WebElement userName = driver.findElement(By.id("my-text-id"));
+    userName.sendKeys("Sai Charan");
+
+    WebElement password = driver.findElement(By.name("my-password"));
+    password.sendKeys("Sai@123");
+
+    WebElement description = driver.findElement(By.name("my-textarea"));
+    description.sendKeys("I am learning Selenium WebDriver");
+
+    Select dropdown = new Select(driver.findElement(By.name("my-select")));
+    dropdown.selectByVisibleText("Three");
+
+    WebElement checkbox = driver.findElement(By.id("my-check-1"));
+    boolean isSelected = checkbox.isSelected();
+    if (checkbox.isSelected) {
+      checkbox.click();
+    }
+
+    WebElement defaultCheckbox = driver.findElement(By.id("my-check-2"));
+    boolean isSelected = defaultCheckbox.isSelected();
+    if (!isSelected) {
+      checkbox.click();
+    }
+
+    driver.findElement(By.id("my-radio-2"))
+
+    driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
     driver.quit();
   }
 }
