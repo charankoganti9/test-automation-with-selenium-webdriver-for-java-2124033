600d1ae636f37287ca46c25190b582a202de4b81
Sai Charan Koganti <charan.koganti@gmail.com>
2025-06-16 11:58:16 -0500

    Detect Visibility Chellenge

--- src/main/java/element_state/Challenge_DetectVisibility.java
+++ src/main/java/element_state/Challenge_DetectVisibility.java
@@ -9,6 +9,17 @@ public class Challenge_DetectVisibility extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/coordinates_tests/page_with_hidden_element.html");
 
+    WebElement box = driver.findElement(By.id("box"));
+
+    if(box.isDisplayed())
+    {
+      box.click();
+    } 
+
+    else{
+      System.out.println("box is not visible");
+    }
+
     /*
      * TODO: add your solution here
      * find the box element
